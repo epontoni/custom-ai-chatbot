23be35fa77ce2b8a7513887ce5e3ddaacdc0c631
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-07-03 13:33:56 -0300

    Review section

--- app/(admin)/view-chatbots/page.tsx
+++ app/(admin)/view-chatbots/page.tsx
@@ -18,7 +18,9 @@ export default async function ViewChatbotsPage() {
   if (!userId) return;
 
   // Get the chatbot for the user
-  const { data: chatbotsByUser } = await serverClient.query<
+  const {
+    data: { chatbotsByUser },
+  } = await serverClient.query<
     GetChatbotsByUserData,
     GetChatbotsByUserDataVariables
   >({
@@ -28,15 +30,13 @@ export default async function ViewChatbotsPage() {
     },
   });
 
-  console.log(">>> DATA: ()", chatbotsByUser, userId);
+  console.log(">>> DATA: ()", chatbotsByUser, typeof chatbotsByUser);
 
-  // const sortedChatbotsByUser: Chatbot[] = [...chatbotsByUser ].sort(
-  //   (a, b) =>
-  //     new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-  // );
+  const sortedChatbotsByUser: Chatbot[] = [...chatbotsByUser].sort(
+    (a, b) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
 
-  // DELETE THIS LINE:
-  const sortedChatbotsByUser = [];
   return (
     <div className="flex-1 pb-20 p-10">
       <h1 className="text-xl lg:text-3xl font-semibold mb-5">
@@ -59,8 +59,8 @@ export default async function ViewChatbotsPage() {
         {sortedChatbotsByUser?.map((chatbot) => {
           return (
             <Link href={`/edit-chatbot/${chatbot.id}`}>
-              <li className="relative p-10 border rounded-md max-w-3xl bg-white">
-                <div>
+              <li className="relative p-10 border rounded-md max-w-3xl bg-white hover:shadow-md hover:bg-gray-100">
+                <div className="flex justify-between items-center">
                   <div className="flex items-center space-x-4">
                     <Avatar seed={chatbot.name} />
                     <h2 className="text-xl font-bold">{chatbot.name}</h2>
@@ -69,6 +69,28 @@ export default async function ViewChatbotsPage() {
                   <p className="absolute top-5 right-5 text-xs text-gray-400">
                     Created: {new Date(chatbot.created_at).toLocaleString()}
                   </p>
+                </div>
+                <hr className="mt-2" />
+                <div className="grid grid-cols-2 gap-10 md:gap-5 p-5">
+                  <h3 className="italic">Characteristics</h3>
+                  <ul className="text-xs">
+                    {chatbot.chatbot_characteristics.length === 0 && (
+                      <p>No Characteristics added yet.</p>
+                    )}
+                    {chatbot.chatbot_characteristics.map((characteristic) => {
+                      return (
+                        <li
+                          className="list-disc break-words"
+                          key={characteristic.id}
+                        >
+                          {characteristic.content}
+                        </li>
+                      );
+                    })}
+                  </ul>
+
+                  <h3 className="italic">No of Sessions</h3>
+                  <p>{chatbot.chat_sessions.length}</p>
                 </div>
               </li>
             </Link>
--- graphql/queries/index.ts
+++ graphql/queries/index.ts
@@ -25,6 +25,31 @@ export const GET_CHATBOT_BY_ID = gql`
   }
 `;
 
+// export const GET_CHATBOT_BY_CLERK_ID = gql`
+//   query GetChatbotByClerkId($clerk_user_id: String!) {
+//     chatbotsList(clerk_user_id: $clerk_user_id) {
+//       id
+//       name
+//       created_at
+//       chatbot_characteristics {
+//         id
+//         content
+//         created_at
+//       }
+//       chat_sessions {
+//         id
+//         created_at
+//         guest_id
+//         messages {
+//           id
+//           content
+//           created_at
+//         }
+//       }
+//     }
+//   }
+// `;
+
 export const GET_CHATBOT_BY_USER = gql`
   query GetChatbotsByUser($clerk_user_id: String!) {
     chatbotsByUser(clerk_user_id: $clerk_user_id) {
@@ -46,6 +71,45 @@ export const GET_CHATBOT_BY_USER = gql`
           content
           created_at
         }
+      }
+    }
+  }
+`;
+
+export const GET_USER_CHATBOTS = gql`
+  query GetUserChatbots($userId: String!) {
+    chatbotsByUser(clerk_user_id: $userId) {
+      id
+      name
+      chat_sessions {
+        id
+        created_at
+        guests {
+          name
+          email
+        }
+      }
+    }
+  }
+`;
+
+export const GET_CHAT_SESSION_MESSAGES = gql`
+  query GetChatSessionMessages($id: Int!) {
+    chat_sessions(id: $id) {
+      id
+      created_at
+      messages {
+        id
+        content
+        created_at
+        sender
+      }
+      chatbots {
+        name
+      }
+      guests {
+        name
+        email
       }
     }
   }
